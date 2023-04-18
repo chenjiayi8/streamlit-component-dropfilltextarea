e9bf87f46cf6e654a71bf8f60ce6ae86b21cd633
Frank <chenjiayi_344@hotmail.com>
2023-04-18 13:32:06 +0200

    Refactor setup.py and MANIFEST.in to version 0.1.1 and update the component description to be more concise and clear

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="streamlit-component-dropfilltextarea",
-    version="0.1.0",
+    version="0.1.1",
     author="Jiayi Chen",
     author_email="chenjiayi_344@hotmail.com",
-    description="Streamlit Component DropFillTextarea allows you to drag and drop files onto a text area, making it easy to fill in large amounts of text quickly. With dropfill_textarea, users can quickly populate text areas with pre-existing text files, reducing manual input and increasing efficiency. The component also offers flexible layout options, allowing users to customize the label and text area's size, position, and other properties. Whether you're a developer or a user, dropfill_textarea is the perfect solution for simplifying your workflow.",
+    description="Streamlit's DropFillTextarea lets users drag and drop files onto a text area, filling in text quickly. It populates text areas with pre-existing files, reducing manual input, while offering layout customization. Ideal for simplifying workflows for both developers and users.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(where="src"),
